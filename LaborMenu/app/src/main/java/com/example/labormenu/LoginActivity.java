8baf00f716929a7cf9ebf048cb5175e691659004
Hyunjin02 <jhj981102@gmail.com>
2021-07-13 20:38:27 +0900

    [add]노동자 메뉴 상단바+틀 만들기

---
+++ LaborMenu/app/src/main/java/com/example/labormenu/LoginActivity.java
@@ -0,0 +1,31 @@
+package com.example.labormenu;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+        Button button = (Button) findViewById(R.id.newActivity);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void goToMain(View view) {
+        Intent intent = new Intent(getApplicationContext(), SelectUserActivity.class);
+        startActivity(intent);
+    }
+}
